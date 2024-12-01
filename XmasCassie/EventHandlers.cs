7c6fffae592b5fa45ab697b3f302f380bc0b1fb9
Ruslan Samburov <samburov.com@gmail.com>
2024-12-01 23:32:50 +0300

    fix

--- XmasCassie/EventHandlers.cs
+++ XmasCassie/EventHandlers.cs
@@ -1,38 +1,18 @@
-﻿using System.Linq;
-using Exiled.API.Features;
-using Exiled.Events.EventArgs.Server; 
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
 using PlayerRoles;
-using Respawning;
 
 namespace XmasCassiePlugin
 {
     public class EventHandlers
     {
-        public void OnRespawningTeam(RespawningTeamEventArgs ev)
+        public void OnRespawnedTeam(RespawnedTeamEventArgs ev)
         {
-            if (ev.NextKnownTeam != SpawnableTeamType.NineTailedFox)
-                return;
-
-            foreach (Player player in ev.Players)
+            if (ev.Wave.TargetFaction != Faction.FoundationStaff)
             {
-                player.UnitName;
+                Cassie.Clear();
+                Cassie.Message("<b>Звенят бубенцы, звенят бубенцы, звенят всю дорогу.</b><split><b>О, как весело кататься в открытых санях с одной лошадью.</b> <size=0> pitch_1 Xmas_JingleBells . pitch_0.9 Xmas_Epsilon11 NATO_A 6 Xmas_HasEntered 4 Xmas_ScpSubjects </size><split><b>Звенят бубенцы, звенят бубенцы, звенят всю дорогу.</b><split><b>О, как весело кататься в открытых санях с одной лошадью.</b><split><b>Тактическое праздничное подразделение <color=#005EBC>Epsilon-11</color>, позначеный как <color=#005EBC>ALPHA-06</color> вошёл в мастерскую.<split><b>Всем оставшимся эльфам рекомендуется укрыться в ближайшем пряничном домике, пока подразделение не завершит праздничную обработку объекта.</b><split><b>Ожидается повторное сдерживания: <color=red>4</color> вредителей, ненавидящих праздники.</b> pitch_1", false, true, true);
             }
-
-            string unitName = ev.Player.UnitName ?? "UNKNOWN";
-            string unitCassie = unitName.Replace("-", "_");
-
-            int scpCount = Player.List.Count(player => player.IsScp && player.Role != RoleTypeId.Scp0492);
-
-            Log.Debug($"Respawning MTF Unit: {unitName} (Cassie: {unitCassie}), SCPs Left: {scpCount}");
-
-            string cassieMessage = Plugin.Singleton.Config.CustomCassieMessage
-                .Replace("{designatedtext}", unitName)
-                .Replace("{designatedcassie}", unitCassie)
-                .Replace("{scpleft}", scpCount.ToString());
-
-            ev.IsAllowed = false;
-            Cassie.Clear();
-            Cassie.Message(cassieMessage, false, true, true);
         }
     }
 }
