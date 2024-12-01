c0afd344788479c87f3506e7d595378ff8596eca
Ruslan Samburov <samburov.com@gmail.com>
2024-12-01 23:41:18 +0300

    fix

--- XmasCassie/EventHandlers.cs
+++ XmasCassie/EventHandlers.cs
@@ -1,6 +1,8 @@
 ﻿using Exiled.API.Features;
 using Exiled.Events.EventArgs.Server;
 using PlayerRoles;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace XmasCassiePlugin
 {
@@ -8,9 +10,20 @@ namespace XmasCassiePlugin
     {
         public void OnRespawnedTeam(RespawnedTeamEventArgs ev)
         {
-            if (ev.Wave.TargetFaction != Faction.FoundationStaff)
+            if (ev.Wave.TargetFaction != Faction.FoundationStaff && ev.Players.Count() != 0)
             {
                 Cassie.Clear();
+                List<RoleTypeId> IgnoredScp = new List<RoleTypeId>()
+                {
+                    RoleTypeId.Scp0492,
+                };
+
+                int scpCount = Player.List.Count(x => x.IsScp && IgnoredScp.Contains(x.Role)); // Тут лучше список использовать
+                Player player = ev.Players.First();
+                string text = "{designatedcassie} и {scpleft}, {designatedtext}"
+                .Replace("{designatedtext}", player.UnitName)
+                .Replace("{scpleft}", scpCount.ToString());
+                
                 Cassie.Message("<b>Звенят бубенцы, звенят бубенцы, звенят всю дорогу.</b><split><b>О, как весело кататься в открытых санях с одной лошадью.</b> <size=0> pitch_1 Xmas_JingleBells . pitch_0.9 Xmas_Epsilon11 NATO_A 6 Xmas_HasEntered 4 Xmas_ScpSubjects </size><split><b>Звенят бубенцы, звенят бубенцы, звенят всю дорогу.</b><split><b>О, как весело кататься в открытых санях с одной лошадью.</b><split><b>Тактическое праздничное подразделение <color=#005EBC>Epsilon-11</color>, позначеный как <color=#005EBC>ALPHA-06</color> вошёл в мастерскую.<split><b>Всем оставшимся эльфам рекомендуется укрыться в ближайшем пряничном домике, пока подразделение не завершит праздничную обработку объекта.</b><split><b>Ожидается повторное сдерживания: <color=red>4</color> вредителей, ненавидящих праздники.</b> pitch_1", false, true, true);
             }
         }
